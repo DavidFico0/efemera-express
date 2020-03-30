4263c2ec82098ebfa3252bdf44eabaa21fdb96a4
DavidFico0 <davidfico22@gmail.com>
2020-03-30 17:42:05 -0300

    Implantação do fs express, controle de arquivos

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const newsLetter = path.join('public', 'json', 'newsLetter.json');
+const contatos = path.join('public', 'json', 'contatos.json');
+
 const homeController = {
     index: (req, res) => {
         let servicos = [
@@ -13,6 +18,53 @@ const homeController = {
         ]
 
         res.render('index', {title: "Home",  listaServicos: servicos, listaBanners:banners});
+    },
+
+    newsletter: (req, res) => {
+        let {email} = req.body;
+        let news = {email};
+        news.email = req.body.email;
+        let newsJson = [];
+        console.log(newsJson);
+
+        if(fs.existsSync(newsLetter)){
+            newsJson = fs.readFileSync(newsLetter, {encoding:'utf-8'});
+            newsJson = JSON.parse(newsJson);
+            newsJson.push(news)
+            newsJson = JSON.stringify(newsJson);
+            fs.writeFileSync(newsLetter, newsJson)
+        } else {
+            newsJson.push(JSON.stringify([news]));
+            fs.writeFileSync(newsLetter, newsJson);
+        }
+        
+        res.render('news-sucess', { title:"Sucesso", email});
+    },
+
+    contato: (req, res) => {
+        let{nome, email, mensagem} = req.body;
+        let infoContato = { nome, email, mensagem};
+        let contatosJson = []
+
+        if(fs.existsSync(contatos)){
+            contatosJson = fs.readFileSync(contatos, {encoding:'utf-8'});
+            contatosJson = JSON.parse(contatosJson);
+            contatosJson.push(infoContato);
+            contatosJson = JSON.stringify(contatosJson);
+            fs.writeFileSync(contatos, contatosJson);
+        } else {
+            contatosJson.push(JSON.stringify([infoContato]))
+            fs.writeFileSync(contatos, contatosJson);
+        }
+
+        res.render('email-sucess', {nome, email, mensagem, title: 'Contato'});
+    },
+
+    listarContatos: (req, res) => {
+        let listaContatos = fs.readFileSync(contatos, {encoding:'utf-8'});
+        listaContatos = JSON.parse(listaContatos);
+
+        res.render('lista-contatos', {listaContatos: listaContatos, title:'Lista de Contatos'})
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
-let express = require('express');
-let router = express.Router();
-let homeController = require('../controller/homeController');
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controller/homeController');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 router.get('/', homeController.index);
+
+router.post('/newsletter', homeController.newsletter);
+
+router.post('/contato', homeController.contato);
+
+router.get('/listacontatos', homeController.listarContatos);
 
 
 module.exports = router;
